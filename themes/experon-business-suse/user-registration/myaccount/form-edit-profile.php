f6eafb5257e77d5cef883edee02d3c4362503e77
Ferenc Szekely <ferenc.szekely@suse.com>
2020-04-15 01:52:54 +0300

    fixes for the country list
    Signed-off-by: Ferenc Szekely <ferenc.szekely@suse.com>

--- themes/experon-business-suse/user-registration/myaccount/form-edit-profile.php
+++ themes/experon-business-suse/user-registration/myaccount/form-edit-profile.php
@@ -23,6 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'user_registration_before_edit_profile_form' ); 
 ?>
 <div class="ur-frontend-form login" id="ur-frontend-form">
+  <script src="/wp-content/themes/experon-business-suse/js/profile.js"></script>
 	<form class="user-registration-EditProfileForm edit-profile" action="" method="post" enctype="multipart/form-data">
 		<div class="ur-form-row">
 			<div class="ur-form-grid">
