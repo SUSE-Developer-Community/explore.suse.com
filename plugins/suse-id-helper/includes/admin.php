a2bfd2eccac26721934fe2db3583596eeec839c7
Ferenc Szekely <ferenc.szekely@suse.com>
2020-06-11 13:48:55 +0300

    improvements after 1st demo
    Signed-off-by: Ferenc Szekely <ferenc.szekely@suse.com>

--- plugins/suse-id-helper/includes/admin.php
+++ plugins/suse-id-helper/includes/admin.php
@@ -32,17 +32,27 @@ function suse_id_admin_page()
     // sanitize and validate POST vars
     // TODO: return error message(s) if some validation fails
     if (isset($_POST, $_POST[$settings_domain],
+      $_POST[$settings_domain]['login_page'],
       $_POST[$settings_domain]['myaccount_page'],
       $_POST[$settings_domain]['sso_login_url'],
       $_POST[$settings_domain]['account_create_url'],
       $_POST[$settings_domain]['account_update_url'],
       $_POST[$settings_domain]['password_reset_url']))
     {
+      // suse_id_login_page
+      // 1. must have value 0 or must be a valid page in Wordpress
+      $raw = $_POST[$settings_domain]['login_page'];
+      $page = get_post($raw);
+      if ($raw == 0 || $page)
+      {
+        update_option($prefix . '_login_page', $raw);
+      }
+
       // suse_id_myaccount_page
-      // 1. must be a valid page in Wordpress
+      // 1. must have value 0 or must be a valid page in Wordpress
       $raw = $_POST[$settings_domain]['myaccount_page'];
       $page = get_post($raw);
-      if ($page)
+      if ($raw == 0 || $page)
       {
         update_option($prefix . '_myaccount_page', $raw);
       }
@@ -55,6 +65,14 @@ function suse_id_admin_page()
       if ($sanitized !== FALSE && strlen($sanitized) <= 255)
       {
         update_option($prefix . '_sso_login_url', $sanitized);
+      }
+
+      // suse_id_login_label
+      // 1. length: max 255 bytes
+      $sanitized = sanitize_text_field($_POST[$settings_domain]['login_label']);
+      if (strlen($sanitized) <= 255)
+      {
+        update_option($prefix . '_login_label', $sanitized);
       }
 
       // suse_id_account_create_url
@@ -88,17 +106,33 @@ function suse_id_admin_page()
       }
     }
 
+    get_option($prefix . '_login_page') ? $selected = get_option($prefix . '_login_page') : $selected = 0;
+    $login_page_args = array(
+      'echo' => 0,
+      'name' => $settings_domain . '[login_page]',
+      'value_field' => 'ID',
+      'selected' => $selected,
+      'show_option_none' => __('no_page', 'suse_id_helper'),
+      'option_none_value' => '0'
+    );
+    $login_page = wp_dropdown_pages($login_page_args);
+
+    get_option($prefix . '_myaccount_page') ? $selected = get_option($prefix . '_myaccount_page') : $selected = 0;
     $myaccount_page_args = array(
       'echo' => 0,
       'name' => $settings_domain . '[myaccount_page]',
       'value_field' => 'ID',
-      'selected' => get_option($prefix . '_myaccount_page')
+      'selected' => $selected,
+      'show_option_none' => __('no_page', 'suse_id_helper'),
+      'option_none_value' => '0'
     );
     $myaccount_page = wp_dropdown_pages($myaccount_page_args);
 
     $settings = array(
-      'sso_login_url' => get_option($prefix . '_sso_login_url'),
+      'login_page' => $login_page,
       'myaccount_page' => $myaccount_page,
+      'sso_login_url' => get_option($prefix . '_sso_login_url'),
+      'login_label' => get_option($prefix . '_login_label'),
       'account_create_url' => get_option($prefix . '_account_create_url'),
       'account_update_url' => get_option($prefix . '_account_update_url'),
       'password_reset_url' => get_option($prefix . '_password_reset_url'),
