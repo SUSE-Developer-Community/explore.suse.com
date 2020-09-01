6020c28c8ca51e7bee2437d72636a98f92b228bd
Ferenc Szekely <ferenc.szekely@suse.com>
2020-09-01 14:04:36 +0300

    latest WIP towards making use of the new onboarding API. Done: create account (except user feedback), account listing, password change.
    Signed-off-by: Ferenc Szekely <ferenc.szekely@suse.com>

--- plugins/sandbox-onboarding/assets/js/cap_user_ui.js
+++ plugins/sandbox-onboarding/assets/js/cap_user_ui.js
@@ -11,28 +11,15 @@ window.onload = (event) => {
           ui.panel.html("Couldn't load information for this tab.");
         });
       },
-      create: function( event, ui ) {
-        // consent prior allowing sandbox account request
-        const cb = document.getElementById('user_consent');
-        
-        // callback for the consent read checkbox
-        function consentClicked() {
-          const button = document.getElementById('request_account');
-          cb.checked ? button.removeAttribute('disabled') : button.setAttribute('disabled', true);
-        }
-        // init callback for the consent read checkbox
-        if (typeof cb != 'undefined' && cb != null) {
-          cb.addEventListener('change', event => {
-            consentClicked();
-          });
-        }
-        // init callback for the Request sandbox account button
-        // todo
+      create: function( event, ui ) {        
+        // init callback for inputs 
+        jQuery('div#newacct input').on('change', validateNewAccount);
       },
       activate: function( event, ui ) {
         // check active tab
         if (ui.newTab.context.hash === '#newacct') {
-          // we already did everything for the 1st tab in 'create'. See above ^
+          // this is the 1st tab and it gets prepared in 'create'. See above ^
+          jQuery('div#accounts div.changepass').hide();
         }
         if (ui.newTab.context.hash === '#accounts') {
           // request sandbox accounts via ajax
@@ -43,27 +30,22 @@ window.onload = (event) => {
           // ajax_object is passed from php via localization; see cap.php
           jQuery.post(ajax_object.ajax_url, data, function(response) {
             try {
-              let rows = JSON.parse(response);
-              console.log('from server: ', rows);
+              console.log(response);
+              let json = JSON.parse(response);
 
-              if (typeof rows === 'undefined' || rows.length == 0) {
-                jQuery('#accounts div.nodata').show();
+              if (json.code == 1 || json.rows.length == 0) {
+                jQuery('#accounts div.notice, #accounts div.notice div.nodata').show();
                 jQuery('#accounts ul.header').hide();
                 jQuery('#accounts ul.data').hide();
               } else {
-                jQuery('#accounts div.nodata').hide();
-                if (ajax_object.show_passive_accounts == true) {
-                  jQuery('#accounts div.passiveinfo').show();
-                } else {
-                  jQuery('#accounts div.passiveinfo').hide();
-                }
+                jQuery('#accounts div.notice, #accounts div.notice div.nodata').fadeOut(200);
                 jQuery('#accounts ul.header').show();
                 jQuery('#accounts ul.data').show();
-                listAccounts(rows);
+                listAccounts(json.rows);
                 initActions();
               }
 
-              jQuery('#accounts div.wait').hide();
+              jQuery('#accounts div.notice div.wait').fadeOut(200);
             } catch (err) {
               console.log('Could not parse server response:', response);
             }
@@ -74,35 +56,82 @@ window.onload = (event) => {
   });
 
   /** 
+   * Validate inputs for new account
+   * Quite lame.. 
+   */
+  function validateNewAccount(event) {
+    const button = document.getElementById('request_account');
+    const cb = document.getElementById('user_consent');
+
+    let account = document.querySelector('div#newacct input#username').value;
+    let password = document.querySelector('div#newacct input#password').value;
+
+    if (account != '' && cb.checked && 
+          password != '' && password.length >= 8) {
+      button.removeAttribute('disabled');
+      button.addEventListener('click', createAccount);
+    } else {
+      button.setAttribute('disabled', true);
+      button.removeEventListener('click', createAccount);
+    }
+  }
+
+  /** 
+   * Callback for the create sandbox account
+   */
+  function createAccount(event) {
+    let account = document.querySelector('div#newacct input#username').value;
+    let password = document.querySelector('div#newacct input#password').value;
+    console.log('submit new acct request: ', account, password);
+
+    // submit new password
+    var data = {
+      'action': 'create_sandbox_account',
+      'account': account,
+      'password': password
+    };
+
+    // ajax_object is passed from php via localization; see cap.php
+    jQuery.post(ajax_object.ajax_url, data, function(response) {
+      try {
+        let result = JSON.parse(response); 
+        
+        console.log(result);
+      } catch (e) {
+
+        console.log('error during password change:', e);
+      }
+    });
+  }
+
+  /** 
    * Populates the list of accounts form the JSON object
    */
-  function listAccounts(rows, show_passive) {
+  function listAccounts(rows) {
     // copy the template row and reuse it while looping through the json
     const template = jQuery("div#accounts ul.data li:first").clone();
     jQuery("div#accounts ul.data li").remove();
+  
+    let i = 0;
 
-    for (const account of rows) {
-
-      if (show_passive == false && account.active == false) {
-        continue;
-      }
+    for (let account of rows) {
+      i++;
 
       let item = jQuery(template).clone();
 
       if (account.active === true) {
         item.addClass('active');
       }
+    
+      (i % 2 == 0) ? item.addClass('even') : item.addClass('odd');
 
       item.children('div.account').text(account.userName)
-
-      let time = new Date(account.lastLogonTime);      
-      item.children('div.lastaccess').text(time.toISOString());
+      item.children('div.lastaccess').text(account.lastLogonTime);
       item.children('div.lastpasschange').text(account.passwordLastModified);
 
       jQuery("div#accounts ul.data").append(item);
 
       item = null;
-      newacct = null;
     }
   }
 
@@ -119,6 +148,8 @@ window.onload = (event) => {
     jQuery('div#accounts ul.data li.active div.actions span.changepass')
       .on('click', function() {
         let account = jQuery(this).parent().siblings('div.account').text();
+        jQuery('div#accounts div.changepass input[type=password]').on('change', validatePassword);
+
         changePassword(account);
       });
   }
@@ -138,11 +169,80 @@ window.onload = (event) => {
    * Callback for the change password action
    */
   function changePassword(account) {
-    console.log('change password: ', account);    
-    // 1. enable password change dialog
-    //    * verify password and password_confirmation are the same values
-    // 2. send password to backend via ajax
-    // 3a show success message or
-    // 3b display error if pass change failed
+    let visible = jQuery('div#accounts div.changepass').is(":visible");
+    if (! visible) {
+      jQuery('div#accounts div.changepass').show();
+    }
+
+    jQuery('div#accounts div.changepass .account').text(account);
+
+    jQuery('div#accounts div.changepass div.content').fadeIn(200);
+    jQuery('div#accounts div.changepass div.response').text('').hide();
+
+    jQuery('div#accounts div.changepass span.close').on('click', function(e, t) {
+      jQuery('div#accounts div.changepass').fadeOut(200);
+    });
+  }
+
+  /**
+   * Callback for validating password values 
+   * Quite lame..
+   */
+  function validatePassword(event) {    
+    let pass1 = document.getElementById('password1');
+    let pass2 = document.getElementById('password2');
+    let button = document.getElementById('change_password');
+    
+    let disabled = true;
+
+    if (pass1.value.length >= 8 && pass2.value.length >= 8) {
+      if (pass1.value == pass2.value) {
+        disabled = false;
+      }
+    }
+
+    if (disabled) {
+      button.setAttribute('disabled', true);
+      button.removeEventListener('click', submitPassword);
+    } else {
+      button.removeAttribute('disabled');
+      button.addEventListener('click', submitPassword);
+    }
+
+    return false;
+  }
+
+  /**
+   * Sends new password via ajax and parses the response
+   */
+  function submitPassword(event) { 
+    let pass1 = document.getElementById('password1');    
+    let account = document.querySelector('div#accounts div.changepass .account');    
+
+    // submit new password
+    var data = {
+      'action': 'change_sandbox_password',
+      'account': account.innerText,
+      'password': pass1.value
+    };
+
+    // ajax_object is passed from php via localization; see cap.php
+    jQuery.post(ajax_object.ajax_url, data, function(response) {
+      try {
+        let result = JSON.parse(response); 
+        jQuery('div#accounts div.changepass div.content').fadeOut(200);
+        jQuery('div#accounts div.changepass div.response').
+          show().
+          text(result.response);
+        jQuery('div#accounts div.changepass').delay(3000).fadeOut(200);
+        
+        console.log(result);
+      } catch (e) {
+        jQuery('div#accounts div.changepass div.response').text('');
+        jQuery('div#accounts div.changepass').hide();
+
+        console.log('error during password change:', e);
+      }
+    });
   }
 }
